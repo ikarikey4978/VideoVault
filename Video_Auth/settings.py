0b1a8ec6c0e475423fd07292bbb6f6654a5c59ba
Kartikey Agrahari <agraharikartikey@gmail.com>
2026-01-15 03:40:30 +0530

    Fix static and media settings for deployment

--- Video_Auth/settings.py
+++ Video_Auth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&g_!!i#$#vxk7mqzv-1^a*z$z+wai3c99(c8sjvjxuy*pq@uw#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -129,7 +129,9 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 STATICFILES_DIRS = [
     BASE_DIR / "Video_Auth" / "static",
+
 ]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 # Default primary key field type
